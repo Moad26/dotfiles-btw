ad4a7e507917d5148ee94e03d2a5e768ad0647c3
Moad26 <moad2632005@gmail.com>
2026-09-13 21:40:08 -0400

    niri

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,6 +76,8 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  programs.niri.enable = true
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
